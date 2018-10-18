5dbcf0da8e84054ce3d2af06d33bf0c3775ef6ee
Daniel Pinto <42196925+dann2207@users.noreply.github.com>
2018-10-18 18:19:51 -0300

    Random funcionando
    Ya lo probé y anda bien. El tema es que tarda mucho, mas o menos M*(2 minutos)
    Yo puse M = 1, pero lo ideal es que m sea grande, así el programa toma más ensayos

--- Random_centrality.py
+++ Random_centrality.py
@@ -1,6 +1,7 @@
 # Random
 import random
 import copy
+import time
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -13,17 +14,24 @@ N = len(nodes)
 X = [0]*(N-1)
 Y = [0]*(N-1)
 
-M = 10
-for j in range(M):
-	G = G_APMS.copy()
-	nod = copy.deepcopy(nodes)
-	for i in range(1,N):
-		n = random.choice(nod)
-		nod.remove(n)
-		x = i/N
-		G.remove_node(n)
-		gig = max(nx.connected_component_subgraphs(G), key=len)
-		y = len(G)/len(G_APMS)
-		X[i].__add__(x/M)
-		Y[i].__add__(y/M)
 
+M = 1
+t0 = time.time()
+for j in range(M):
+    G = G_APMS.copy()
+    nod = copy.deepcopy(nodes)
+    for i in range(1,N):
+        n = random.choice(list(nod))
+        nod.remove(n)
+        x = i/N
+        G.remove_node(n)
+        gig = max(nx.connected_component_subgraphs(G), key=len)
+        y = len(gig)/len(G_APMS)
+        X[i-1] += x/M
+        Y[i-1] += y/M
+
+t = time.time()
+print(t-t0)
+
+plt.plot(X,Y)
+plt.show()
